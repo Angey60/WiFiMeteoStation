35ef3b1a1e6e3814cd8c7eb88398240c90bd7cdd
Андрей Борисов <angey60@icloud.com>
2025-08-31 11:51:46 +0300

    Начало

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,10 @@ void setClock()
   // Для работы TLS-соединения нужны корректные дата и время, получаем их с NTP серверов
   configTime("MSK-3", "pool.ntp.org", "time.nist.gov");
 
-  if (DEBUG) {Serial.print(F("Waiting for NTP time sync: "));}
+  if (DEBUG)
+  {
+    Serial.print(F("Waiting for NTP time sync: "));
+  }
   int i = 0;
   time_t now = time(nullptr);
   while (now < 1000000000)
@@ -68,38 +71,114 @@ void setClock()
       // Если в течение этого времени не удалось подключиться - выходим с false
       // Бескорнечно ждать подключения опасно - если подключение было разорвано во время работы
       // нужно всё равно "обслуживать" реле и датчики, иначе может случиться беда
-      if (DEBUG) {DEBUG_SERIAL.println();}
-      if (DEBUG) {DEBUG_SERIAL.println(F("Time sync failed!"));}
+      if (DEBUG)
+      {
+        DEBUG_SERIAL.println();
+      }
+      if (DEBUG)
+      {
+        DEBUG_SERIAL.println(F("Time sync failed!"));
+      }
       // return false;
     };
-    if (DEBUG) {DEBUG_SERIAL.print(F("."));}
+    if (DEBUG)
+    {
+      DEBUG_SERIAL.print(F("."));
+    }
     delay(500);
   }
 
   // Время успешно синхронизировано, выводим его в монитор порта
   // Только для целей отладки, этот блок можно исключить
-  if (DEBUG) {DEBUG_SERIAL.println(F(" ok"));}
+  if (DEBUG)
+  {
+    DEBUG_SERIAL.println(F(" ok"));
+  }
 
   struct tm timeinfo;
   localtime_r(&now, &timeinfo);
 
-  if (DEBUG) {DEBUG_SERIAL.print(F("Current time: "));}
-  if (DEBUG) {DEBUG_SERIAL.print(asctime(&timeinfo));}
+  if (DEBUG)
+  {
+    DEBUG_SERIAL.print(F("Current time: "));
+  }
+  if (DEBUG)
+  {
+    DEBUG_SERIAL.print(asctime(&timeinfo));
+  }
 }
 
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   // int result = myFunction(2, 3);
+  DEBUG_SERIAL.begin(DEBUG_SERIAL_BAUDRATE);
+  while (!DEBUG_SERIAL)
+  {
+    ;
+  }
+  delay(500);
+
+  if (DEBUG)
+  {
+    DEBUG_SERIAL.println();
+    DEBUG_SERIAL.println(F("Device started ..."));
+    DEBUG_SERIAL.println();
+    DEBUG_SERIAL.flush();
+    delay(500);
+  }
+
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASS);
+  if (DEBUG)
+  {
+    DEBUG_SERIAL.print("Connecting to WiFi ...");
+  }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    if (DEBUG)
+    {
+      DEBUG_SERIAL.print(".");
+    }
+    delay(500);
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    if (DEBUG)
+    {
+      DEBUG_SERIAL.println(" ok");
+    }
+    // Корректируем дату и время
+    setClock();
+  }
+  else
+  {
+    if (DEBUG)
+    {
+      DEBUG_SERIAL.println(" no!!!");
+    }
+  }
+
+  if (DEBUG)
+  {
+    DEBUG_SERIAL.println();
+  }
+
+  delay(1000);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
