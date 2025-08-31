5b5af4a0364286fc3931c38c0f70fcfce4c18a43
Андрей Борисов <angey60@icloud.com>
2025-08-31 15:07:53 +0300

    otaStart

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,10 @@ const char *PASS = "98797899";
 #define DEBUG true
 
 // https://github.com/Angey60/WiFiMeteoStation/blob/main/firmware.bin
+// https://angey60meteo.ru/firmware.bin
 //
-// GitHub
-const char IRG_Root_X1[] PROGMEM = R"CERT(
+// GitHub.com
+/*const char IRG_Root_X1[] PROGMEM = R"CERT(
 -----BEGIN CERTIFICATE-----
 MIIEoTCCBEigAwIBAgIRAKtmhrVie+gFloITMBKGSfUwCgYIKoZIzj0EAwIwgY8x
 CzAJBgNVBAYTAkdCMRswGQYDVQQIExJHcmVhdGVyIE1hbmNoZXN0ZXIxEDAOBgNV
@@ -45,9 +46,43 @@ A1dL881tXbMnMCUGA1UdEQQeMByCCmdpdGh1Yi5jb22CDnd3dy5naXRodWIuY29t
 MAoGCCqGSM49BAMCA0cAMEQCIHGMp27BBBJ1356lCe2WYyzYIp/fAONQM3AkeE/f
 ym0sAiBtVfN3YgIZ+neHEfwcRhhz4uDpc8F+tKmtceWJSicMkA==
 -----END CERTIFICATE-----
+)CERT";*/
+// sweb.ru
+const char IRG_Root_X1[] PROGMEM = R"CERT(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
 )CERT";
 // Iot Core
-static const char ISRG_Root_x1[] PROGMEM = R"EOF(
+static const char ISRG_Root_X2[] PROGMEM = R"EOF(
 -----BEGIN PRIVATE KEY-----
 MIIJQgIBADANBgkqhkiG9w0BAQEFAASCCSwwggkoAgEAAoICAQCS4xK3tScrgzRX
 xQeGt4Y5ADny5An6PFNIXZ3DxIM4N9qwa+0wJSUWyMLlqF45aZOA0h5zxTOL3i03
@@ -104,8 +139,9 @@ Yp9Mc3Aw+tlOEP3BXArGwUVaIgUpu8RX1yTyL1SfyXI9iF+Q7mlcSWUWfwC/tvyB
 
 // Create a list of certificates with the server certificate
 X509List cert(IRG_Root_X1);
+BearSSL::X509List certISRG(ISRG_Root_X2);
 
-BearSSL::X509List certISRG(ISRG_Root_x1);
+String firmware_url = "https://angey60meteo.ru/firmware.bin";
 
 // Set time via NTP, as required for x.509 validation
 void setClock()
@@ -185,8 +221,8 @@ void update_error(int err)
   DEBUG_SERIAL.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
 }
 
-//CALLBACK:  HTTP update fatal error code -101
-//HTTP_UPDATE_FAILD Error (-101): Server Did Not Report Size
+// CALLBACK:  HTTP update fatal error code -101
+// HTTP_UPDATE_FAILD Error (-101): Server Did Not Report Size
 void otaStart(const char *linkOTA)
 {
   DEBUG_SERIAL.print("OTA :: Receiving OTA: ");
@@ -195,6 +231,7 @@ void otaStart(const char *linkOTA)
 
   // Корректируем дату и время
   setClock();
+  // Запускаем обновление прошивки
   WiFiClientSecure otaWiFi;
   otaWiFi.setTrustAnchors(&cert);
 
@@ -205,9 +242,8 @@ void otaStart(const char *linkOTA)
   ESPhttpUpdate.onProgress(update_progress);
   ESPhttpUpdate.onError(update_error);
 
-  /*
   // Запускаем обновление
-  t_httpUpdate_return ret = ESPhttpUpdate.update(otaWiFi, "http://github.com/Angey60/WiFiMeteoStation/blob/main/firmware.bin");
+  t_httpUpdate_return ret = ESPhttpUpdate.update(otaWiFi, linkOTA);
   // Анализируем результат
   switch (ret)
   {
@@ -224,7 +260,6 @@ void otaStart(const char *linkOTA)
     break;
   };
   Serial.println("");
-  */
 }
 
 // put function declarations here:
@@ -273,7 +308,7 @@ void setup()
       DEBUG_SERIAL.println(" ok");
     }
     // Запускаем обновление
-    otaStart("https://github.com/Angey60/WiFiMeteoStation/blob/main/firmware.bin");
+    //otaStart(firmware_url.c_str());
   }
   else
   {
